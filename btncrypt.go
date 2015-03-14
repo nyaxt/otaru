ea012166e7b173168623f6d0503457bfa831acb8
Kouhei Ueno <ueno@nyaxtstep.com>
2015-03-15 08:36:37 +0900

    btncrypt: BtnEncryptWrite seems ok. Needs to update decrypt

--- btncrypt.go
+++ btncrypt.go
@@ -4,13 +4,19 @@ package otaru
 // This code has not gone through any security audit, so don't trust this code / otaru encryption.
 
 import (
+	"bytes"
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
 	"fmt"
+	"io"
 )
 
-func randomNonce(size int) []byte {
+const (
+	BtnFrameMaxPayload = 256 * 1024
+)
+
+func RandomBytes(size int) []byte {
 	nonce := make([]byte, size)
 	var l int
 	l, err := rand.Read(nonce)
@@ -44,10 +50,136 @@ func Encrypt(key, plain []byte) ([]byte, error) {
 	}
 
 	nonceSize := gcm.NonceSize()
-	nonce := randomNonce(nonceSize)
+	nonce := RandomBytes(nonceSize)
 
 	envelope := gcm.Seal(nonce, nonce, plain, nil)
 	return envelope, nil
+}
+
+type frameEncryptor struct {
+	gcm       cipher.AEAD
+	b         bytes.Buffer
+	encrypted []byte
+}
+
+func newFrameEncryptor(gcm cipher.AEAD) *frameEncryptor {
+	return &frameEncryptor{
+		gcm:       gcm,
+		encrypted: make([]byte, 0, gcm.NonceSize()+BtnFrameMaxPayload+gcm.Overhead()),
+	}
+}
+
+func (f *frameEncryptor) Write(p []byte) (int, error) {
+	if _, err := f.b.Write(p); err != nil {
+		panic(err)
+	}
+
+	return len(p), nil
+}
+
+func (f *frameEncryptor) Written() int {
+	return f.b.Len()
+}
+
+func (f *frameEncryptor) CapacityLeft() int {
+	return BtnFrameMaxPayload - f.b.Len()
+}
+
+func (f *frameEncryptor) Flush() ([]byte, error) {
+	if f.Written() > BtnFrameMaxPayload {
+		return nil, fmt.Errorf("frame payload size exceeding max len: %d > %d", f.Written(), BtnFrameMaxPayload)
+	}
+
+	nonce := RandomBytes(f.gcm.NonceSize())
+
+	f.encrypted = f.encrypted[:len(nonce)]
+	copy(f.encrypted, nonce)
+
+	f.encrypted = f.gcm.Seal(f.encrypted, nonce, f.b.Bytes(), nil)
+	f.b.Reset()
+	return f.encrypted, nil
+}
+
+type BtnEncryptWriteCloser struct {
+	target   io.Writer
+	key      []byte
+	lenTotal int
+	written  int
+	*frameEncryptor
+}
+
+func NewBtnEncryptWriteCloser(target io.Writer, key []byte, lenTotal int) (*BtnEncryptWriteCloser, error) {
+	gcm, err := gcmFromKey(key)
+	if err != nil {
+		return nil, err
+	}
+
+	bew := &BtnEncryptWriteCloser{
+		target:         target,
+		key:            key,
+		lenTotal:       lenTotal,
+		written:        0,
+		frameEncryptor: newFrameEncryptor(gcm),
+	}
+	return bew, nil
+}
+
+func intMin(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func (bew *BtnEncryptWriteCloser) flushFrame() error {
+	frame, err := bew.frameEncryptor.Flush()
+	if err != nil {
+		return err
+	}
+	if _, err := bew.target.Write(frame); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (bew *BtnEncryptWriteCloser) Write(p []byte) (int, error) {
+	if len(p) == 0 {
+		return 0, nil
+	}
+
+	left := p
+	for len(left) > 0 {
+		framePayloadLen := intMin(bew.frameEncryptor.CapacityLeft(), len(p))
+		framePayload := left[:framePayloadLen]
+		if _, err := bew.frameEncryptor.Write(framePayload); err != nil {
+			panic(err)
+		}
+		left = left[framePayloadLen:]
+
+		if bew.frameEncryptor.CapacityLeft() == 0 {
+			if err := bew.flushFrame(); err != nil {
+				return 0, err
+			}
+		}
+		if bew.frameEncryptor.CapacityLeft() == 0 {
+			panic("flushFrame should brought back capacity")
+		}
+	}
+
+	bew.written += len(p)
+	return len(p), nil
+}
+
+func (bew *BtnEncryptWriteCloser) Close() error {
+	if bew.lenTotal != bew.written {
+		return fmt.Errorf("Incomplete data written")
+	}
+
+	if err := bew.flushFrame(); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func Decrypt(key, envelope []byte) ([]byte, error) {
--- btncrypt_test.go
+++ btncrypt_test.go
@@ -1,12 +1,15 @@
 package otaru
 
 import (
+	"bytes"
+	"io"
 	"testing"
 )
 
-func TestEncrypt(t *testing.T) {
+func TestEncrypt_Short(t *testing.T) {
 	key := []byte("0123456789abcdef")
-	envelope, err := Encrypt(key, []byte("0123456789abcdef"))
+	payload := []byte("short string")
+	envelope, err := Encrypt(key, payload)
 	if err != nil {
 		t.Errorf("Failed to encrypt: %v", err)
 	}
@@ -16,5 +19,90 @@ func TestEncrypt(t *testing.T) {
 		t.Errorf("Failed to decrypt: %v", err)
 	}
 
-	t.Errorf("content: %v", string(plain))
+	if !bytes.Equal(plain, payload) {
+		t.Errorf("Failed to restore original payload")
+	}
+}
+
+func TestEncrypt_Long(t *testing.T) {
+	key := []byte("0123456789abcdef")
+	payload := RandomBytes(1024 * 1024)
+	envelope, err := Encrypt(key, payload)
+	if err != nil {
+		t.Errorf("Failed to encrypt: %v", err)
+	}
+
+	plain, err := Decrypt(key, envelope)
+	if err != nil {
+		t.Errorf("Failed to decrypt: %v", err)
+	}
+
+	if !bytes.Equal(payload, plain) {
+		t.Errorf("Failed to restore original payload")
+	}
+}
+
+func verifyWrite(t *testing.T, w io.Writer, payload []byte) {
+	n, err := w.Write(payload)
+	if err != nil {
+		t.Errorf("Failed to write data to BtnEncryptWriter: %v", err)
+	}
+	if n != len(payload) {
+		t.Errorf("bew.Write returned n != len(p)")
+	}
+}
+
+func TestBtnEncryptWriter_WriteAtOnce(t *testing.T) {
+	key := []byte("0123456789abcdef")
+	//payload := RandomBytes(1024 * 1024)
+	payload := []byte("short string")
+
+	var b bytes.Buffer
+	bew, err := NewBtnEncryptWriteCloser(&b, key, len(payload))
+	if err != nil {
+		t.Errorf("Failed to create BtnEncryptWriter: %v", err)
+	}
+
+	verifyWrite(t, bew, payload)
+	if err := bew.Close(); err != nil {
+		t.Errorf("bew.Close failed: %v", err)
+	}
+
+	plain, err := Decrypt(key, b.Bytes())
+	if err != nil {
+		t.Errorf("Failed to decrypt: %v", err)
+	}
+
+	if !bytes.Equal(payload, plain) {
+		t.Errorf("Failed to restore original payload")
+	}
+}
+
+func TestBtnEncryptWriter_PartialWrite(t *testing.T) {
+	key := []byte("0123456789abcdef")
+	payload := RandomBytes(1024 * 1024)
+
+	var b bytes.Buffer
+	bew, err := NewBtnEncryptWriteCloser(&b, key, len(payload))
+	if err != nil {
+		t.Errorf("Failed to create BtnEncryptWriter: %v", err)
+	}
+
+	verifyWrite(t, bew, payload[:3])
+	verifyWrite(t, bew, payload[3:1024])
+	verifyWrite(t, bew, payload[1024:4096])
+	verifyWrite(t, bew, payload[4096:])
+
+	if err := bew.Close(); err != nil {
+		t.Errorf("bew.Close failed: %v", err)
+	}
+
+	plain, err := Decrypt(key, b.Bytes())
+	if err != nil {
+		t.Errorf("Failed to decrypt: %v", err)
+	}
+
+	if !bytes.Equal(payload, plain) {
+		t.Errorf("Failed to restore original payload")
+	}
 }
