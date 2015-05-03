7c8ed2e9880753e541f3a5f14c57d35506fa52bd
nyaxt <ueno@nyaxtstep.com>
2015-05-03 15:00:20 +0900

    fuse/filesystem_test.go: TestServeFUSE_WriteReadFile

--- fuse/filesystem_test.go
+++ fuse/filesystem_test.go
@@ -1,7 +1,10 @@
 package fuse_test
 
 import (
+	"bytes"
+	"io/ioutil"
 	"log"
+	"path"
 	"testing"
 
 	bfuse "bazil.org/fuse"
@@ -11,15 +14,12 @@ import (
 	. "github.com/nyaxt/otaru/testutils"
 )
 
-func TestServeFUSE(t *testing.T) {
+func fusetestCommon(t *testing.T, fs *otaru.FileSystem, f func(mountpoint string)) {
 	bfuse.Debug = func(msg interface{}) {
 		log.Printf("fusedbg: %v", msg)
 	}
 
 	mountpoint := "/tmp/hoge"
-
-	bs := TestFileBlobStore()
-	fs := otaru.NewFileSystemEmpty(bs, TestCipher())
 
 	done := make(chan bool)
 	ready := make(chan bool)
@@ -30,11 +30,40 @@ func TestServeFUSE(t *testing.T) {
 		}
 		close(done)
 	}()
-
 	<-ready
+
+	f(mountpoint)
+
 	if err := bfuse.Unmount(mountpoint); err != nil {
 		t.Errorf("umount failed: %v", err)
 	}
-
 	<-done
+}
+
+func TestServeFUSE_DoNothing(t *testing.T) {
+	bs := TestFileBlobStore()
+	fs := otaru.NewFileSystemEmpty(bs, TestCipher())
+
+	fusetestCommon(t, fs, func(mountpoint string) {})
+}
+
+func TestServeFUSE_WriteReadFile(t *testing.T) {
+	bs := TestFileBlobStore()
+	fs := otaru.NewFileSystemEmpty(bs, TestCipher())
+
+	fusetestCommon(t, fs, func(mountpoint string) {
+		if err := ioutil.WriteFile(path.Join(mountpoint, "hello.txt"), HelloWorld, 0644); err != nil {
+			t.Errorf("failed to write file: %v", err)
+		}
+	})
+
+	fusetestCommon(t, fs, func(mountpoint string) {
+		b, err := ioutil.ReadFile(path.Join(mountpoint, "hello.txt"))
+		if err != nil {
+			t.Errorf("Failed to read file: %v", err)
+		}
+		if !bytes.Equal(HelloWorld, b) {
+			t.Errorf("Content mismatch!: %v", err)
+		}
+	})
 }
