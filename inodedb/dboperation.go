673e50b96421fb44fe82dbc600425435f7873bb2
Kouhei Ueno <kouhei@chromium.org>
2015-05-22 11:50:15 +0900

    move DBOperation to dboperation.go

---
+++ inodedb/dboperation.go
@@ -0,0 +1,121 @@
+package inodedb
+
+import (
+	"fmt"
+)
+
+type DBOperation interface {
+	Apply(s *DBState) error
+}
+
+type OpMeta struct {
+	Kind string `json:"kind"`
+}
+
+type InitializeFileSystemOp struct {
+	OpMeta `json:",inline"`
+}
+
+var _ = DBOperation(&InitializeFileSystemOp{})
+
+func (op *InitializeFileSystemOp) Apply(s *DBState) error {
+	if len(s.nodes) != 0 {
+		return fmt.Errorf("DB not empty. Already contains %d nodes!", len(s.nodes))
+	}
+	if s.lastID != 0 {
+		return fmt.Errorf("DB lastId != 0")
+	}
+
+	n := &DirNode{
+		INodeCommon: INodeCommon{ID: 1, OrigPath: "/"},
+		Entries:     make(map[string]ID),
+	}
+
+	if err := s.addNewNode(n); err != nil {
+		return fmt.Errorf("Failed to create root DirNode: %v", err)
+	}
+	s.lastID = 1
+
+	return nil
+}
+
+type CreateDirOp struct {
+	OpMeta   `json:",inline"`
+	NodeLock `json:"nodelock"`
+	OrigPath string `json:"origpath"`
+}
+
+var _ = DBOperation(&CreateDirOp{})
+
+func (op *CreateDirOp) Apply(s *DBState) error {
+	if err := s.checkLock(op.NodeLock, true); err != nil {
+		return err
+	}
+
+	n := &DirNode{
+		INodeCommon: INodeCommon{ID: op.ID, OrigPath: op.OrigPath},
+		Entries:     make(map[string]ID),
+	}
+
+	if err := s.addNewNode(n); err != nil {
+		return fmt.Errorf("Failed to create new DirNode: %v", err)
+	}
+
+	return nil
+}
+
+type CreateFileOp struct {
+	OpMeta   `json:",inline"`
+	NodeLock `json:"nodelock"`
+	OrigPath string `json:"origpath"`
+}
+
+func (op *CreateFileOp) Apply(s *DBState) error {
+	if err := s.checkLock(op.NodeLock, true); err != nil {
+		return err
+	}
+
+	n := &FileNode{
+		INodeCommon: INodeCommon{ID: op.ID, OrigPath: op.OrigPath},
+		Size:        0,
+	}
+
+	if err := s.addNewNode(n); err != nil {
+		return fmt.Errorf("Failed to create new FileNode: %v", err)
+	}
+
+	return nil
+}
+
+type HardLinkOp struct {
+	OpMeta   `json:",inline"`
+	NodeLock `json:"nodelock"`
+	Name     string `json:"name"`
+	TargetID ID     `json:"targetid"`
+}
+
+func (op *HardLinkOp) Apply(s *DBState) error {
+	if err := s.checkLock(op.NodeLock, false); err != nil {
+		return err
+	}
+
+	n, ok := s.nodes[op.ID]
+	if !ok {
+		return ENOENT
+	}
+	dn, ok := n.(*DirNode)
+	if !ok {
+		return ENOTDIR
+	}
+
+	if _, ok := s.nodes[op.TargetID]; !ok {
+		return ENOENT
+	}
+
+	if _, ok := dn.Entries[op.Name]; ok {
+		return EEXIST
+	}
+	dn.Entries[op.Name] = op.TargetID
+
+	return nil
+}
