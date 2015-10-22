3c7ad6a637fda720d0958537c6622fa0a3d9da97
Kouhei Ueno <kouhei@chromium.org>
2015-10-22 21:13:39 +0900

    add test for OpenFile leak #39

--- filesystem_test.go
+++ filesystem_test.go
@@ -45,3 +45,36 @@ func TestFileWriteRead(t *testing.T) {
 		t.Errorf("PRead content != PWrite content: %v", buf)
 	}
 }
+
+func TestFile_CloseOpenFile(t *testing.T) {
+	snapshotio := inodedb.NewSimpleDBStateSnapshotIO()
+	txio := inodedb.NewSimpleDBTransactionLogIO()
+	idb, err := inodedb.NewEmptyDB(snapshotio, txio)
+	if err != nil {
+		t.Errorf("NewEmptyDB failed: %v", err)
+		return
+	}
+
+	bs := tu.TestFileBlobStore()
+	fs := otaru.NewFileSystem(idb, bs, tu.TestCipher())
+	h, err := fs.OpenFileFullPath("/hello.txt", flags.O_CREATE|flags.O_RDWR, 0666)
+	if err != nil {
+		t.Errorf("OpenFileFullPath failed: %v", err)
+		return
+	}
+
+	if err = h.PWrite(tu.HelloWorld, 0); err != nil {
+		t.Errorf("PWrite failed: %v", err)
+		return
+	}
+
+	if stats := fs.GetStats(); stats.NumOpenFiles != 1 {
+		t.Errorf("NumOpenFiles %d != 1", stats.NumOpenFiles)
+	}
+
+	h.Close()
+
+	if stats := fs.GetStats(); stats.NumOpenFiles != 0 {
+		t.Errorf("NumOpenFiles %d != 0", stats.NumOpenFiles)
+	}
+}
