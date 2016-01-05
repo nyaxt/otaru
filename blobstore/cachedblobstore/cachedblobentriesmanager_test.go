4efaa4759e53283e994c12aa59b7fb84c50c7516
kouhei <kouhei@lion.home.nyaxtstep.com>
2016-01-05 22:16:38 +0900

    test FindAllSyncable

--- blobstore/cachedblobstore/cachedblobentriesmanager_test.go
+++ blobstore/cachedblobstore/cachedblobentriesmanager_test.go
@@ -2,10 +2,12 @@ package cachedblobstore_test
 
 import (
 	"reflect"
+	"sort"
 	"testing"
 	"time"
 
 	"github.com/nyaxt/otaru/blobstore/cachedblobstore"
+	"github.com/nyaxt/otaru/util"
 )
 
 func TestCachedBlobEntriesManager_RunQuit(t *testing.T) {
@@ -14,7 +16,16 @@ func TestCachedBlobEntriesManager_RunQuit(t *testing.T) {
 	mgr.Quit()
 }
 
-func TestCachedBlobEntriesManager_FindAllSyncable(t *testing.T) {
+func syncable2blobpaths(scs []util.Syncer) []string {
+	bps := make([]string, 0, len(scs))
+	for _, s := range scs {
+		bp := s.(*cachedblobstore.CachedBlobEntry).BlobPath()
+		bps = append(bps, bp)
+	}
+	return bps
+}
+
+func TestCachedBlobEntriesManager_FindSync(t *testing.T) {
 	cachedblobstore.DisableAutoSyncForTesting = true
 	defer func() { cachedblobstore.DisableAutoSyncForTesting = false }()
 
@@ -74,14 +85,30 @@ func TestCachedBlobEntriesManager_FindAllSyncable(t *testing.T) {
 		be.InitializeForTesting(cachedblobstore.CacheEntryWritebackInProgress, now.Add(-330*time.Second), now.Add(-330*time.Second))
 	}
 
-	scs := mgr.FindSyncCandidates(10)
-	bps := make([]string, 0, len(scs))
-	for _, s := range scs {
-		bp := s.(*cachedblobstore.CachedBlobEntry).BlobPath()
-		bps = append(bps, bp)
+	{
+		scs := mgr.FindSyncCandidates(10)
+		expected := []string{"dirtySuperOld", "writeInProgressOldSync", "dirtyWrite5"}
+		bps := syncable2blobpaths(scs)
+		if !reflect.DeepEqual(expected, bps) {
+			t.Errorf("FindSyncCandidates(10) Expected: %+v, Actual: %+v", expected, bps)
+		}
 	}
-	expected := []string{"dirtySuperOld", "writeInProgressOldSync", "dirtyWrite5"}
-	if !reflect.DeepEqual(expected, bps) {
-		t.Errorf("Expected: %+v, Actual: %+v", expected, bps)
+	{
+		scs := mgr.FindSyncCandidates(1)
+		expected := []string{"dirtySuperOld"}
+		bps := syncable2blobpaths(scs)
+		if !reflect.DeepEqual(expected, bps) {
+			t.Errorf("FindSyncCandidates(1) Expected: %+v, Actual: %+v", expected, bps)
+		}
+	}
+	{
+		scs := mgr.FindAllSyncable()
+		expected := []string{"dirtySuperOld", "writeInProgressOldSync", "dirtyWrite5", "dirtyRecentWrite", "dirtyRecentSync"}
+		sort.Strings(expected)
+		bps := syncable2blobpaths(scs)
+		sort.Strings(bps)
+		if !reflect.DeepEqual(expected, bps) {
+			t.Errorf("FindAllSyncable Expected: %+v, Actual: %+v", expected, bps)
+		}
 	}
 }
