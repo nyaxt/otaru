5458cf50d260aa7b7e5a9208aaa4a3658cccedf6
nyaxt <ueno@nyaxtstep.com>
2016-01-16 10:16:54 +0900

    /api/debug/pprof/set_block_profile_rate

--- mgmt/mdebug/debughandler.go
+++ mgmt/mdebug/debughandler.go
@@ -3,6 +3,7 @@ package mdebug
 import (
 	"net/http"
 	"net/http/pprof"
+	"runtime"
 
 	"github.com/gorilla/mux"
 
@@ -13,6 +14,12 @@ func Install(srv *mgmt.Server) {
 	rtr := srv.APIRouter().PathPrefix("/debug/pprof").Subrouter()
 
 	rtr.HandleFunc("/", http.HandlerFunc(pprof.Index))
+	rtr.HandleFunc("/set_block_profile_rate", func(w http.ResponseWriter, req *http.Request) {
+		runtime.SetBlockProfileRate(1)
+
+		w.Header().Set("Content-Type", "text/plain")
+		w.Write([]byte("ok"))
+	})
 	rtr.HandleFunc("/cmdline", http.HandlerFunc(pprof.Cmdline))
 	rtr.HandleFunc("/profile", http.HandlerFunc(pprof.Profile))
 	rtr.HandleFunc("/symbol", http.HandlerFunc(pprof.Symbol))
